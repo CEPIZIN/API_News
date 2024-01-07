d740238fba362bd38f232cea34fe9cf82a84c597
CEPIZIN~ <lauramathias03@gmail.com>
2024-01-07 01:58:56 -0300

    Gson update

--- main/mainSearch.java
+++ main/mainSearch.java
@@ -1,21 +1,31 @@
 package main;
 
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class mainSearch {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scr = new Scanner(System.in);
+        System.out.println("Enter your search: ");
+        var topic = scr.nextLine();
+
+        String URL = "https://newsapi.org/v2/everything?q=" + topic + "&apiKey=c196de70f8ca483ea848fdf786285e90";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-        //Search for news articles that mention a specific topic or keyword
-                .uri(URI.create("https://newsapi.org/v2/everything?q=bitcoin&apiKey=c196de70f8ca483ea848fdf786285e90"))
+                .uri(URI.create(URL))
                 .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
-    }
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
+        System.out.println(response.body());
+
+        Gson gson = new Gson();
+        //gson.
+    }
 }
